7b2ed42c9b5caacb4a506db5bb89b5fb36f68997
sackeovas <120979637+sackeovas@users.noreply.github.com>
2023-05-24 14:32:15 +0000

    Basic note

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const newNoteButton = document.getElementById("newNoteButton")
+const notes = document.getElementById("notes")
+newNoteButton.addEventListener("click",function(){
+    notes.classList.add("note")
+    console.log("Adding new note.")
+})
 
