6ab10c3b4eaccaef5902f826384665cdde3561ef
sackeovas <120979637+sackeovas@users.noreply.github.com>
2023-06-05 18:32:03 +0000

    mess

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const newNoteButton = document.getElementById("newNoteButton")
-const notesArea = document.getElementById("notesArea")
+const note = document.getElementById("note")
 const textArea = document.getElementById("textArea")
+
 newNoteButton.addEventListener("click",function(){
     const newNote = document.createElement("textArea")
     newNote.rows = 10;
-    notesArea.append(newNote)
+    note.append(newNote)
     console.log("Adding new note.")
-    newNote.classList.add("noteClass")
+    newNote.classList.add("noteTextArea")
 })
+
 
